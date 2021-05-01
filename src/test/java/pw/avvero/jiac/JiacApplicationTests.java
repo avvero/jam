6508fd11c30098175b1b76cb089da859b3c32e32
fxdev-belyaev-ay <anton.belyaev@sibit.tech>
2021-05-01 20:42:27 +0700

    Initialized

---
+++ src/test/java/pw/avvero/jiac/JiacApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.avvero.jiac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
