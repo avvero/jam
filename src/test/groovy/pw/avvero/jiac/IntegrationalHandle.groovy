c0c19dec6166718d9933d13ab5a09dfa5de4cd11
fxdev-belyaev-ay <anton.belyaev@sibit.tech>
2021-05-05 10:16:08 +0700

    Integrational handle is provided

---
+++ src/test/groovy/pw/avvero/jiac/IntegrationalHandle.groovy
@@ -0,0 +1,26 @@
+package pw.avvero.jiac
+
+import pw.avvero.jiac.dsl.SchemaWriter
+import pw.avvero.jiac.jira.JiraIssue
+import pw.avvero.jiac.jira.JiraIssueMapper
+import pw.avvero.test.SerializationUtils
+import spock.lang.Ignore
+import spock.lang.Specification
+
+import static pw.avvero.test.ResourceDataProvider.fromFile
+
+@Ignore
+class IntegrationalHandle extends Specification {
+
+    def "Parse jira issue file and print schema to sout"() {
+        when:
+        def issue = JiraIssueMapper.map(SerializationUtils.read(fromFile(file), JiraIssue))
+        def schema = SchemaWriter.toString(issue)
+        new ConsoleWriter().newLineBlue(schema)
+        then:
+        1 == 1
+        where:
+        file = "jira/xxx.json"
+    }
+
+}
